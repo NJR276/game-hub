cd1d08860819dcf08b0db990bc62b71f98bb58d9
NJR276 <nrothchild@mockingbirdsystems.com>
2023-05-08 20:20:40 -0500

    Generic Data Hook

--- CapstoneWebsite/Capstone/src/hooks/useGenres.ts
+++ CapstoneWebsite/Capstone/src/hooks/useGenres.ts
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 
 export interface Genre {
@@ -8,39 +6,9 @@ export interface Genre {
     name: string;
 }
 
-interface FetchGenresResponse{
-    count:number;
-    results: Genre[]
-}
 
 
-const useGrenres = () =>{
-    const [genres, setGenres] = useState<Genre[]>([]);
-    const [error, setError] = useState("");
-    const [isLoading, setLoading] = useState(false);
-  
-    useEffect(() => {
-      const controller = new AbortController();
-      
-      setLoading(true);
-      apiClient
-        .get<FetchGenresResponse>("/genres", {signal: controller.signal})
-        .then((res) => {
-            setGenres(res.data.results);
-            setLoading(false);
-        })
-        .catch((err) => {
-            if(err instanceof CanceledError) return; 
-                setError(err.message); 
-                setLoading(false);
-            });
-
-        return () => controller.abort();
-    }, []);
-    
-
-    return { genres, error, isLoading };
-}
+const useGrenres = () => useData<Genre>("/genres")
 
 
 export default useGrenres;
